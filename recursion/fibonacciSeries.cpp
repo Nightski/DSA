35d4f8be80a012becb587b203a4660abc33b5292
Nightski <101112649+Nightski@users.noreply.github.com>
2024-09-28 17:09:15 +0530

    Create fibonacciSeries.cpp

---
+++ recursion/fibonacciSeries.cpp
@@ -0,0 +1,21 @@
+/* the following code is fibonacci using recursion
+   the code take 2 power n complexity which is way to high
+   but it is the simplest one 
+   we can optimize it using memoization
+*/
+#include<iostream>
+
+int fibo(int n){
+    if (n == 1 || n == 2) return 1;
+    return fibo(n - 1) + fibo(n - 2);
+}
+
+int main(){
+    int n;
+    std::cout<<"Enter Number: ";
+    std::cin>>n;
+    for(int i=1;i<=n;i++){
+        std::cout<<fibo(i)<<std::endl;
+    }
+    return 0;
+}

0f0e777b8f290f24f5ad9878b5ae83ad71c578ff
Nightski <101112649+Nightski@users.noreply.github.com>
2024-10-01 21:37:14 +0530

    fibonnaci series

--- recursion/fibonacciSeries.cpp
+++ recursion/fibonacciSeries.cpp
@@ -2,6 +2,8 @@
    the code take 2 power n complexity which is way to high
    but it is the simplest one 
    we can optimize it using memoization
+
+   Time complexity O(2^n) where n is the range of the series
 */
 #include<iostream>
 

f2f18d55301aea6a3b419d05d6196a04cb050cec
Nightski <101112649+Nightski@users.noreply.github.com>
2024-10-01 21:36:16 +0530

    factorial

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -1,3 +1,11 @@
+/*A factorial is the multiplication of series of number from the given to upto 1
+ example 4 factorial if 4*3*2*1
+ here we have the base case as if n = 1 then return 1
+ else multiply the current element with the previous element until 1 recursively
+ 
+ Time complexity O(n), where n is the number*/
+
+
 #include<iostream>
 
 int fact(int n){

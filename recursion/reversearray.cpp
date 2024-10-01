66702f810432a3db6512662965f38de7b70212ec
Nightski <101112649+Nightski@users.noreply.github.com>
2024-10-01 21:42:45 +0530

    reverse an array

--- recursion/reversearray.cpp
+++ recursion/reversearray.cpp
@@ -1,3 +1,16 @@
+/*You are given an array, print it in reverse
+
+ here we have passed a pointer to the array and the size
+ we return when the size if zero i.e. you can no longer traverse the array
+ else we update the pointer to next and decrease one size
+ when the base case hits the last call is printed which is the last element in the array
+ printing the array in reverse
+
+ Time complexity O(n), where n is the size of array
+ */
+
+
+
 #include<iostream>
 
 void rev(int *ar, int size){

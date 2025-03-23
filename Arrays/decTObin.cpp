a3360b50719a8d6a007898b08613aa81143ae2e8
Kevin Vinsent <101112649+Nightski@users.noreply.github.com>
2025-03-23 18:15:42 +0530

    Decimal to Binary

---
+++ Arrays/decTObin.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n = 10;
+    vector<int> rem;
+    while(n != 0){
+        rem.push_back(n % 2);
+        n /= 2;
+    }
+    reverse(rem.begin(), rem.end());
+    for(int num: rem){
+        cout<<num;
+    }
+    return 0;
+}

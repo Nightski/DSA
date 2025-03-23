9851f9b1fd05baf9ae29045737eb05ccb26c2dfb
Kevin Vinsent <101112649+Nightski@users.noreply.github.com>
2025-03-23 19:09:36 +0530

    No change

--- Arrays/decTObin.cpp
+++ Arrays/decTObin.cpp
@@ -18,24 +18,3 @@ int main(){
     }
     return 0;
 }
-
-//no extra space and 10 power concept with and also
-#include<iostream>
-#include<algorithm>
-#include<math.h>
-#include<vector>
-using namespace std;
-
-int main(){
-    int n = 10;
-    int ans = 0, i = 0;
-    int bit;
-    while(n != 0){
-        bit = n & 1;
-        ans = (pow(10,i) * bit) + ans;
-        n = n>>1;
-        i++;
-    }
-    cout<<ans;
-    return 0;
-}
